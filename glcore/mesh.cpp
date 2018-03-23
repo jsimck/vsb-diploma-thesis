69ee4d692280ddf3cf93b7b871def2cab648b9c4
jansimecek <simecekjann@gmail.com>
2018-03-23 13:53:11 +0100

    Added initial opengl files, updated cmake

---
+++ glcore/mesh.cpp
@@ -0,0 +1,111 @@
+#include "mesh.h"
+
+namespace tless {
+    void Mesh::loadFromPly(const std::string &plyFile) {
+        try {
+            // Read the file and create a std::istringstream suitable
+            // for the lib -- tinyply does not perform any file i/o.
+            std::ifstream ss(plyFile, std::ios::binary);
+
+            if (ss.fail()) {
+                throw std::runtime_error("failed to open " + plyFile);
+            }
+
+            // Parse header
+            tinyply::PlyFile file;
+            file.parse_header(ss);
+            std::shared_ptr<tinyply::PlyData> plyVertices, plyNormals, plyFaces;
+
+            // Extract header information
+            try { plyVertices = file.request_properties_from_element("vertex", {"x", "y", "z"}); }
+            catch (const std::exception &e) { std::cerr << "tinyply exception: " << e.what() << std::endl; }
+
+            try { plyNormals = file.request_properties_from_element("vertex", {"nx", "ny", "nz"}); }
+            catch (const std::exception &e) { std::cerr << "tinyply exception: " << e.what() << std::endl; }
+
+            try { plyFaces = file.request_properties_from_element("face", {"vertex_indices"}); }
+            catch (const std::exception &e) { std::cerr << "tinyply exception: " << e.what() << std::endl; }
+
+            file.read(ss);
+
+            // Reserve array size to the size of the buffer
+            std::vector<glm::vec3> _normals(plyVertices->count);
+            std::vector<glm::vec3> _vertices(plyVertices->count);
+            std::vector<glm::ivec3> _indices(plyFaces->count);
+
+            // Copy data from buffer to local arrays
+            std::memcpy(_indices.data(), plyFaces->buffer.get(), plyFaces->buffer.size_bytes());
+            std::memcpy(_vertices.data(), plyVertices->buffer.get(), plyVertices->buffer.size_bytes());
+            std::memcpy(_normals.data(), plyNormals->buffer.get(), plyNormals->buffer.size_bytes());
+
+            // Save vertices and normals to local arrays
+            for (int i = 0; i < _vertices.size(); ++i) {
+                vertices.emplace_back(Vertex(_vertices[i], _normals[i]));
+            }
+
+            // Save incides to local arrays
+            for (auto &ind : _indices) {
+                indices.push_back(static_cast<unsigned int &&>(ind.x));
+                indices.push_back(static_cast<unsigned int &&>(ind.y));
+                indices.push_back(static_cast<unsigned int &&>(ind.z));
+            }
+        } catch (const std::exception &e) {
+            std::cerr << "Caught tinyply exception: " << e.what() << std::endl;
+        }
+    }
+
+    void Mesh::init()  {
+        // Create buffers/arrays
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+        glGenBuffers(1, &EBO);
+
+        // Load data into vertex buffers
+        glBindVertexArray(VAO);
+        // Bind vertex positions and normals
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+
+        // Bind face indices
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+
+        // Set the vertex attribute pointers
+        // Vertex Positions
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *) 0);
+        // Vertex normals
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *) offsetof(Vertex, normal));
+
+        // Unbind current VAO
+        glBindVertexArray(0);
+    }
+
+    Mesh::Mesh(const std::string &plyFile) {
+        loadFromPly(plyFile);
+        init();
+    }
+
+    void Mesh::draw() {
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(indices.size()), GL_UNSIGNED_INT, (void*) 0);
+        glBindVertexArray(0);
+    }
+
+    std::ostream &tless::operator<<(std::ostream &os, const Mesh &mesh) {
+        os << "VBO: " << mesh.VBO
+           << " EBO: " << mesh.EBO
+           << " VAO: " << mesh.VAO << std::endl;
+
+        for (int i = 0; i < mesh.vertices.size(); ++i) {
+            os << "V: " << mesh.vertices[i] <<std::endl;
+        }
+
+        for (int i = 0; i < mesh.indices.size(); i += 3) {
+            os << "V: (" << mesh.vertices[i] << ", " << mesh.vertices[i + 1] << ", " << mesh.vertices[i + 2] << ")" << std::endl;
+        }
+
+        return os;
+    }
+}
---
+++ glcore/mesh.h
@@ -0,0 +1,64 @@
+#ifndef VSB_SEMESTRAL_PROJECT_MESH_H
+#define VSB_SEMESTRAL_PROJECT_MESH_H
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+#include "../libs/tinyply.h"
+
+namespace tless {
+    struct Vertex {
+        glm::vec3 position;
+        glm::vec3 normal;
+
+        Vertex(glm::vec3 position, glm::vec3 normal) : position(position), normal(normal) {}
+
+        friend std::ostream &operator<<(std::ostream &os, const Vertex &vertex) {
+            os << "pos("
+               << vertex.position.x << ", "
+               << vertex.position.y << ", "
+               << vertex.position.z << ") n("
+               << vertex.normal.x << ", "
+               << vertex.normal.y << ", "
+               << vertex.normal.z << ")";
+
+            return os;
+        }
+    };
+
+    class Mesh {
+    private:
+        GLuint VBO, EBO;
+
+        /**
+         * @brief Parses mesh from .ply file using tinyply library.
+         *
+         * @param[in] plyFile Path to the .ply file to parse mesh from
+         */
+        void loadFromPly(const std::string &plyFile);
+
+        /**
+         * @brief Initializes all the VAOs, VBOs for OpenGL.
+         */
+        void init();
+
+    public:
+        GLuint VAO;
+        std::vector<Vertex> vertices;
+        std::vector<unsigned int> indices;
+
+        explicit Mesh(const std::string &plyFile);
+
+        /**
+         * @brief Binds initialized VAOs and draws mesh using glDrawElements.
+         */
+        void draw();
+
+        friend std::ostream &operator<<(std::ostream &os, const Mesh &mesh);
+    };
+}
+
+#endif
